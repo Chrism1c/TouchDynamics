31c4221c38fef9e688415229cc0f908ff4b84af7
chrism1c <christian-miccolis@libero.it>
2021-07-03 21:52:17 +0200

    Aggiunto WekaArff dataset

--- src/DG150/NeuralNetwork_DG150.py
+++ src/DG150/NeuralNetwork_DG150.py
@@ -53,6 +53,27 @@ def calulate_model(index_dataset, clasx, EPOCHS, nodes):
     X['user_id'] = X['user_id'].astype(int)
     y = X['user_id'].values
 
+    SOF_brokens = ["mn_dt", "mx_dt", "am_dt", "qm_dt", "hm_dt", "gm_dt", "md_dt", "rg_dt", "vr_dt", "sd_dt", "sk_dt",
+                   "ku_dt", "fq_dt", "tq_dt", "ir_dt", "ma_dt", "mi_dt", "cv_dt", "se_dt", "mn_ft1", "mx_ft1", "am_ft1",
+                   "qm_ft1", "hm_ft1", "gm_ft1", "md_ft1", "rg_ft1", "vr_ft1", "sd_ft1", "sk_ft1", "ku_ft1", "fq_ft1",
+                   "tq_ft1", "ir_ft1", "ma_ft1", "mi_ft1", "cv_ft1", "se_ft1", "mn_ft2", "mx_ft2", "am_ft2", "qm_ft2",
+                   "hm_ft2", "gm_ft2", "md_ft2", "rg_ft2", "vr_ft2", "sd_ft2", "sk_ft2", "ku_ft2", "fq_ft2", "tq_ft2",
+                   "ir_ft2", "ma_ft2", "mi_ft2", "cv_ft2", "se_ft2", "mn_ft3", "mx_ft3", "am_ft3", "qm_ft3", "hm_ft3",
+                   "gm_ft3", "md_ft3", "rg_ft3", "vr_ft3", "sd_ft3", "sk_ft3", "ku_ft3", "fq_ft3", "tq_ft3", "ir_ft3",
+                   "ma_ft3", "mi_ft3", "cv_ft3", "se_ft3", "mn_ft4", "mx_ft4", "am_ft4", "qm_ft4", "hm_ft4", "gm_ft4",
+                   "md_ft4", "rg_ft4", "vr_ft4", "sd_ft4", "sk_ft4", "ku_ft4", "fq_ft4", "tq_ft4", "ir_ft4", "ma_ft4",
+                   "mi_ft4", "cv_ft4", "se_ft4", "mn_it", "mx_it", "am_it", "qm_it", "hm_it", "gm_it", "md_it", "rg_it",
+                   "vr_it", "sd_it", "sk_it", "ku_it", "fq_it", "tq_it", "ir_it", "ma_it", "mi_it", "cv_it", "se_it",
+                   "mn_ps", "mx_ps", "am_ps", "qm_ps", "hm_ps", "gm_ps", "md_ps", "rg_ps", "vr_ps", "sd_ps", "sk_ps",
+                   "ku_ps", "fq_ps", "tq_ps", "ir_ps", "ma_ps", "mi_ps", "cv_ps", "se_ps"]
+
+    X = X.drop(columns=SOF_brokens)
+
+    print(X.shape)
+    print(list(X.columns))
+
+    # input()
+
     # d = Counter(y)
     # print('--> Class ', d)
 
@@ -62,24 +83,24 @@ def calulate_model(index_dataset, clasx, EPOCHS, nodes):
 
     y = [0 if val == clasx else 1 for val in y]
 
-    # # # OVERSAMPLING
-    # # summarize class distribution
-    # print(Counter(y))
-    # # sample = RandomOverSampler(sampling_strategy='minority')
+    # # OVERSAMPLING
+    # summarize class distribution
+    print(Counter(y))
+    sample = RandomOverSampler(sampling_strategy='minority')
     # sample = RandomUnderSampler(sampling_strategy='majority')
-    # # fit and apply the transform
-    # X_over, y_over = sample.fit_resample(X, y)
-    #
-    # ## summarize class distribution
-    # print(Counter(y_over))
-    #
-    # X = X_over
-    # Y = pd.get_dummies(y_over).values
-    # # print(X)
-    # # print(Y)
+    # fit and apply the transform
+    X_over, y_over = sample.fit_resample(X, y)
+
+    ## summarize class distribution
+    print(Counter(y_over))
+
+    X = X_over
+    Y = pd.get_dummies(y_over).values
+    # print(X)
+    # print(Y)
 
     # SENZA OVERSAMPLING
-    Y = pd.get_dummies(y).values
+    # Y = pd.get_dummies(y).values
 
     n_classes = Y.shape[1]
     print('n_classes: ', n_classes)
@@ -178,25 +199,28 @@ def calulate_model(index_dataset, clasx, EPOCHS, nodes):
 
 
 if __name__ == '__main__':
-
     unique_subjects = np.unique(
-        pd.read_pickle(Path_DG150_FE + '/' + "FE_dataset_pickle_" + datasets_DG150[0] + ".pkl")['user_id'].values.astype(int))
+        pd.read_pickle(Path_DG150_FE + '/' + "FE_dataset_pickle_" + datasets_DG150[0] + ".pkl")[
+            'user_id'].values.astype(int))
 
     unique_subjects = [0, 1]
 
-    EPOCHS = 2
+    EPOCHS = 10
     NODES = 300
-    # clasx = 1
+    clasx = 1
     # db = 0     # 0 - 5
 
     results = pd.DataFrame(columns=['clase', 'eer', 'auc'])
 
     ix = 0
 
-    for index_dataset in range(1):
-        for clasx in unique_subjects:
-            eer2, auc2 = calulate_model(index_dataset, clasx, EPOCHS, NODES)
-            print(clasx, eer2, auc2)
-            results.loc[clasx] = [clasx, eer2, auc2]
-        results.to_csv(PathDG150_Results + '/' + datasets_DG150[index_dataset] + "_Results_NN" + ".csv")
-        results.drop(results.index, inplace=True)
+    # for index_dataset in range(2):
+    #     for clasx in unique_subjects:
+    #         eer2, auc2 = calulate_model(index_dataset, clasx, EPOCHS, NODES)
+    #         print(clasx, eer2, auc2)
+    #         results.loc[clasx] = [clasx, eer2, auc2]
+    #     results.to_csv(PathDG150_Results + '/' + datasets_DG150[index_dataset] + "_Results_NN" + ".csv")
+    #     results.drop(results.index, inplace=True)
+
+    eer2, auc2 = calulate_model(0, clasx, EPOCHS, NODES)
+    print(clasx, eer2, auc2)
---
+++ src/WekaArf/RF_Knn_SVM_WekaArf.py
@@ -0,0 +1,131 @@
+import glob
+from collections import Counter
+
+import matplotlib.pyplot as plt
+from scipy.io import arff
+import pandas as pd
+from sklearn import svm
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.model_selection import train_test_split
+from src.EER import evaluateEER, evaluateEER2
+from src.functions import randomforest, conf_matrix, evaluate, KNNx
+from src.WekaArf.WekaArf_management import Path_WekaArf, Path_WekaArf_Results, subjects
+from src.support import get_df_from_arff
+from imblearn.under_sampling import RandomUnderSampler
+from imblearn.over_sampling import RandomOverSampler
+import os
+
+# index = 0
+# clasx = 600
+
+if __name__ == '__main__':
+
+    datasets_WekaArf = os.listdir(Path_WekaArf)
+    print(datasets_WekaArf)
+
+    id_database_WekaArff = 0 # 0-4
+
+    # subjects = [100, 101]
+
+    " Parametri classificatori "
+    listBootstrap = [0.5, 0.6, 0.7, 0.8, 0.9]
+    listRandomization = ["sqrt", "log2"]
+    listN_estimators = [10, 20, 30]
+    kfolds = 5
+    n_neighbors = 3
+
+    results = pd.DataFrame(columns=['rf_eer', 'knn_eer', 'svm_eer'])
+
+    db = datasets_WekaArf[id_database_WekaArff]
+    for clasx in subjects:
+        data = get_df_from_arff(Path_WekaArf + '/' + db)
+
+        print("----> Clasx: ", clasx, " dataset: ", db)
+
+        data['user_id'] = pd.to_numeric(data['user_id'])
+        subjects = data['user_id'].values
+        # print(data)
+        # print(data.head())
+
+        # Binarizzazione del dataset
+        y = [0 if val == clasx else 1 for val in subjects]
+
+        ### OVERSAMPLIG classe Minoritaria / DOWNSAMPLING classe Maggioritaria
+
+        # summarize class distribution
+        # print(Counter(y))
+        # define undersample strategy
+        sample = RandomOverSampler(sampling_strategy='minority')
+        # sample = RandomUnderSampler(sampling_strategy='majority')
+
+        # fit and apply the transform
+        X_over, y_over = sample.fit_resample(data.iloc[:, :-1], y)
+
+        # summarize class distribution
+        # print(Counter(y_over))
+
+        X = X_over
+        Y = y_over
+
+        ### SENZA OVERSAMPLING
+        # X = data.iloc[:, :-1]
+        # Y = y
+
+        # print(Counter(Y))
+
+        "Train Test split"
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=1)
+
+        n_estimators = listN_estimators[0]
+        randomization = listRandomization[0]
+        bootstrap = listBootstrap[0]
+
+        "Addestramento classificatori"
+        rf = randomforest(X, Y, n_estimators, randomization, bootstrap)
+        neigh = KNNx(X, Y, n_neighbors)
+        SVM = svm.LinearSVC(dual=False).fit(X, Y)
+
+        "Predizioni dei classificatori sul TestSet"
+        res_onTest_rf = rf.predict(X_test)
+        res_onTest_knn = neigh.predict(X_test)
+        res_onTest_SVM = SVM.predict(X_test)
+
+        # "Evaluate sul TestSet"
+        # metrics_rf = evaluate(X_test, Y_test, rf)
+        # metrics_knn = evaluate(X_test, Y_test, neigh)
+        # metrics_svm = evaluate(X_test, Y_test, SVM)
+        # print("\nMetrics RF ----> ", metrics_rf)
+        # print("\nMetrics Knn ----> ", metrics_knn)
+        # print("\nMetrics Svm ----> ", metrics_svm)
+
+        " EER "
+        # print(Y_test, "   ", res_onTest_rf)
+        rf_eer, rf_roc_auc = evaluateEER2(Y_test, res_onTest_rf)
+        print("Rf eer_threshold: ", rf_eer)
+
+        # print(Y_test, "   ", res_onTest_knn)
+        knn_eer, knn_roc_auc = evaluateEER2(Y_test, res_onTest_knn)
+        print("knn eer_threshold: ", knn_eer)
+
+        svm_eer, svmroc_auc = evaluateEER2(Y_test, res_onTest_SVM)
+        print("svm eer_threshold: ", svm_eer)
+
+        results.loc[clasx] = [rf_eer[0], knn_eer[0], svm_eer[0]]
+
+        "Output delle tre matrici di confusione"
+        # conf_matrix(Y_test, res_onTest_rf)
+        # conf_matrix(Y_test, res_onTest_knn)
+        # conf_matrix(Y_test, res_onTest_SVM)
+
+        "Classification Report"
+        # report = classification_report(Y_test, res_onTest_rf)
+        # print(report)
+        #
+        # report = classification_report(Y_test, res_onTest_knn)
+        # print(report)
+        #
+        # report = classification_report(Y_test, res_onTest_SVM)
+        # print(report)
+
+    results.to_csv(Path_WekaArf_Results + '/' + db.replace('.arff', '_Results_Shellow') + ".csv")
+    # results.drop(results.index, inplace=True)
