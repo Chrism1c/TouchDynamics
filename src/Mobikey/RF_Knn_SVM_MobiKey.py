97d9a05edd416ad244b236e97ae64014d1c90608
chrism1c <christian-miccolis@libero.it>
2021-07-07 13:07:38 +0200

    Risultati Shallow MobiKey

--- src/Mobikey/RF_Knn_SVM_MobiKey.py
+++ src/Mobikey/RF_Knn_SVM_MobiKey.py
@@ -10,24 +10,26 @@ if __name__ == '__main__':
     print(datasets_WekaArf)
 
     "Parametri Dataset"
-    indice_database_MobiKey = 4  # #  0-1-2-3-4-5
+    indice_database_MobiKey = 0  # #  0-1-2-3-4-5
     db_name = datasets_WekaArf[indice_database_MobiKey]
 
     Test_Size = 0.1
 
     "Parametri Generali "
     NFold = 10
-    Strategy = 'OverSampler'  # 'UnderSampler'
+    Strategy = 'UnderSampler'  # 'OverSampler'
 
     "Knn"
-    n_neighbors = 3
+    n_neighbors = 1
 
     "Random Forest"
     n_estimators = listN_estimators[3]
     randomization = listRandomization[0]
     bootstrap = listBootstrap[0]
 
-    results = pd.DataFrame(columns=['rf_eer', 'knn_eer', 'svm_eer'])
+    results = pd.DataFrame(columns=['rf_eer', 'rf_oa', 'rf_ba',
+                                    'knn_eer', 'knn_oa', 'knn_ba',
+                                    'svm_eer', 'svm_oa', 'svm_ba'])
 
     for clasx in subjects:
         X, subjects = load_data(indice_database_MobiKey)
@@ -36,8 +38,17 @@ if __name__ == '__main__':
         avgTest_result = RF_kNN_SVM(X.drop(columns=['user_id']), subjects, db_name, clasx, Strategy, NFold, Test_Size,
                                     n_estimators, randomization, bootstrap, n_neighbors)
 
-        results.loc[clasx] = [avgTest_result[0], avgTest_result[1], avgTest_result[2]]
+        results.loc[clasx] = [avgTest_result[0], avgTest_result[1], avgTest_result[2],
+                              avgTest_result[3], avgTest_result[4], avgTest_result[5],
+                              avgTest_result[6], avgTest_result[7], avgTest_result[8]]
 
-    results.loc['AVG'] = [results['rf_eer'].mean(), results['knn_eer'].mean(), results['svm_eer'].mean()]
-    print("RESULTS AVG = ", results['rf_eer'].mean(), results['knn_eer'].mean(), results['svm_eer'].mean())
-    results.to_csv(Path_MobiKey_Results + '/' + db_name.replace('.arff', '_Results_Shellow') + ".csv")
+    results.loc['AVG'] = [
+        results['rf_eer'].mean(), results['rf_oa'].mean(), results['rf_ba'].mean(),
+        results['knn_eer'].mean(), results['knn_oa'].mean(), results['knn_ba'].mean(),
+        results['svm_eer'].mean(), results['svm_oa'].mean(), results['svm_ba'].mean(),
+    ]
+    print("RESULTS AVG = ", results['rf_eer'].mean(), results['rf_oa'].mean(), results['rf_ba'].mean(),
+          results['knn_eer'].mean(), results['knn_oa'].mean(), results['knn_ba'].mean(),
+          results['svm_eer'].mean(), results['svm_oa'].mean(), results['svm_ba'].mean(),
+          )
+    results.to_csv(Path_MobiKey_Results + '/' + db_name.replace('.arff', '_Results_' + Strategy + '_Shellow') + ".csv")
